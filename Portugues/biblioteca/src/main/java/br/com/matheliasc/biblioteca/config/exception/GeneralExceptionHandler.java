02546af0a293b4af1c3898998bb894991c04826b
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2022-08-06 18:07:32 -0300

    Reorganização de packages util e exception

---
+++ Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/config/exception/GeneralExceptionHandler.java
@@ -0,0 +1,9 @@
+package br.com.matheliasc.biblioteca.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    public void handle(MethodArgumentNotValidException.)
+}
--- Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/exception/ExceptionHandler.java
+++
@@ -1,4 +0,0 @@
-package br.com.matheliasc.biblioteca.exception;
-
-public class ExceptionHandler {
-}

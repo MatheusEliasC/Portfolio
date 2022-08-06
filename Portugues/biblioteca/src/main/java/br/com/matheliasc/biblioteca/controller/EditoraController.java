5015028797317e98d86ff2b33e51647a6bfc5e13
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2022-08-06 18:28:44 -0300

    Adição do método GET /editoras

--- Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/controller/EditoraController.java
+++ Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/controller/EditoraController.java
@@ -1,14 +1,26 @@
 package br.com.matheliasc.biblioteca.controller;
 
+import br.com.matheliasc.biblioteca.controller.dto.EditoraDTO;
+import br.com.matheliasc.biblioteca.model.Editora;
 import br.com.matheliasc.biblioteca.repository.EditoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping("/editoras")
 public class EditoraController {
 
     @Autowired
     private EditoraRepository editoraRepository;
 
-    
+    @GetMapping
+    public List<EditoraDTO> listaEditora(){
+        List<Editora> listaEditoras = editoraRepository.findAll();
+        return listaEditoras.stream().map(EditoraDTO::new).collect(Collectors.toList());
+    }
 }

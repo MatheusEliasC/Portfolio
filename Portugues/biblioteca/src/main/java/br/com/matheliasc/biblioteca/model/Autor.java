99a002e44c8b28fab88d88fa3e9e8204f0d9233e
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2022-08-06 18:55:50 -0300

    Remoção de imports genéricos

--- Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/model/Autor.java
+++ Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/model/Autor.java
@@ -1,9 +1,16 @@
 package br.com.matheliasc.biblioteca.model;
 
 import br.com.matheliasc.biblioteca.model.enums.GeneroAutor;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
--- Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/model/Editora.java
+++ Portugues/biblioteca/src/main/java/br/com/matheliasc/biblioteca/model/Editora.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
